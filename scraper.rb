7b265944ec8c32ce8e51ab44b3e362442184da4b
Katie Hoffman <ktahoffman@gmail.com>
2014-02-17 11:19:04 -0500

    removes crawl.rb, replaces code of scraper.rb, adds trial.txt which has the results if line 39 in scraper reads 'while counter < 4'

--- scraper.rb
+++ scraper.rb
@@ -1,50 +1,50 @@
 require 'nokogiri'
 require 'open-uri'
-require 'debugger'
 
 class Scraper
 	attr_accessor :array
-  attr_reader :index_url
+  attr_reader :url
 
-	def initialize(url)
+	def initialize
 		@array = []
-		@index_url = url
+		@url = 'http://www.buzzfeed.com/index/paging?r=1&p='
 	end
 
-	def get_fb_data(article_url)
-		article_html = Nokogiri::HTML(open(article_url))
-    article_xpath = Nokogiri::XML(open(article_url))
-    likes = article_xpath.xpath('//*[@id="u_0_7"]')
-    shares = article_html.search(".num").children[0].text
-    array = [likes, shares]
-    debugger
-    puts "hi"
+	def get_fb_shares(article_url)
+    Nokogiri::HTML(open(article_url)).search('.num').children[0].text
 	end
 
-	def get_headlines_and_urls
-		h2 = Nokogiri::HTML(open(index_url)).search("h2")
+	def get_data(index_url)
+		h2 = Nokogiri::HTML(open(index_url)).search('h2')
     counter = 0
 		h2.each do |e|
 			title = e.text
 			items = /\A(\d+).*/.match(title)
 			if items
-        url = e.children[0].attributes["href"].value
-        data = get_fb_data("http://www.buzzfeed.com" + url)
-				array<< { :title => title,
+        url = 'http://www.buzzfeed.com' + e.children[0].attributes['href'].value
+				array << {:title => title,
 									:items => items[1],
 									:url => url,
-									:likes => data[0],
-                  :shares => data[1]
+                  :shares => get_fb_shares(url)
 								}
         counter += 1
-        puts "fetching likes: #{counter}"
-				sleep(1)
+        puts counter.to_s
 			end
 		end
 		array
 	end
 
+  def increment
+    counter = 1
+    while counter < 3742
+      puts 'index: ' + counter.to_s
+      get_data(url + counter.to_s)
+      counter += 1
+    end
+    array
+  end
+
 end
 
-my_scraper = Scraper.new('http://www.buzzfeed.com/index/paging?r=1&p=1')
-puts my_scraper.get_headlines_and_urls.inspect
+my_scraper = Scraper.new
+puts my_scraper.increment.inspect
