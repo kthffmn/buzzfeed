7f6df72512320071941b7849e52984f7ada584ad
Katie Hoffman <ktahoffman@gmail.com>
2014-02-17 22:20:40 -0500

    fix indentation

--- scraper.rb
+++ scraper.rb
@@ -2,16 +2,16 @@ require 'nokogiri'
 require 'open-uri'
 
 class Scraper
-	attr_accessor :array, :inaccessible
+  attr_accessor :array, :inaccessible
   attr_reader :url, :agent
 
-	def initialize
-		@array = []
-		@url = 'http://www.buzzfeed.com/index/paging?r=1&p='
+  def initialize
+    @array = []
+    @url = 'http://www.buzzfeed.com/index/paging?r=1&p='
     @inaccessible = []
-	end
+  end
 
-	def get_fb_shares(article_url)
+  def get_fb_shares(article_url)
     html = Nokogiri::HTML(open(article_url))
     num = html.search('.num').children[0]
     if num
@@ -20,29 +20,29 @@ class Scraper
       inaccessible << article_url
       'inaccessible'
     end
-	end
+  end
 
-	def get_data(index_url)
-		h2 = Nokogiri::HTML(open(index_url)).search('h2')
+  def get_data(index_url)
+    h2 = Nokogiri::HTML(open(index_url)).search('h2')
     counter = 0
-		h2.each do |e|
-			title = e.text
-			items = /\A(\d+).*/.match(title)
-      the_items = /The (\d+).*/.match(title)
-			if items || the_items
-        if items
-          num = items[1]
-        else
-          num = the_items[1]
-        end
-        url = 'http://www.buzzfeed.com' + e.children[0].attributes['href'].value
-				array << {:title => title, :items => num, :url => url, :shares => get_fb_shares(url)}
-        counter += 1
-        puts counter.to_s
-			end
-		end
-		array
-	end
+    h2.each do |e|
+    title = e.text
+    items = /\A(\d+).*/.match(title)
+    the_items = /The (\d+).*/.match(title)
+    if items || the_items
+      if items
+        num = items[1]
+      else
+        num = the_items[1]
+      end
+      url = 'http://www.buzzfeed.com' + e.children[0].attributes['href'].value
+      array << {:title => title, :items => num, :url => url, :shares => get_fb_shares(url)}
+      counter += 1
+      puts counter.to_s
+      end
+    end
+    array
+  end
 
   def increment
     counter = 1
