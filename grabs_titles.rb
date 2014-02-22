b756e5d6cdd222f37f4c339d9ff955bd1f70ef63
Katie Hoffman <ktahoffman@gmail.com>
2014-02-22 16:29:41 -0500

    updates text.txt so that escape '\' don't appear

--- grabs_titles.rb
+++ grabs_titles.rb
@@ -5,7 +5,7 @@ def create_array_of_titles(array)
   array.each do |hash|
     space_in_front << " " + hash[:title] + "."
   end
-  titles = space_in_front[1..-1].gsub('\"', '"')
+  titles = space_in_front[1..-1].gsub('\"', '"').gsub('\"', '"')
 end
 
-puts create_array_of_titles(array).inspect
+print create_array_of_titles(array)
