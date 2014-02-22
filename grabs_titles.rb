df813d91d51abea584b535cdc28245e742d338c2
Katie Hoffman <ktahoffman@gmail.com>
2014-02-22 16:02:39 -0500

    deletes fake array

--- grabs_titles.rb
+++ grabs_titles.rb
@@ -1,6 +1,4 @@
 
-buzzfeed_array = [{:title=>"35 Nerdy Cards Against Humanity Cards To Add To Your Deck", :items=>"35", :url=>"http://www.buzzfeed.com/kmallikarjuna/nerdy-cards-against-humanity", :shares=>"4K"}, {:title=>"23-Year-Old Woman Grows Beard, Says She Feels More Feminine Than Ever", :items=>"23", :url=>"http://www.buzzfeed.com/alanwhite/23-year-old-woman-grows-beard-says-she-feels-more-feminine-t", :shares=>"230,361"}]
-
 def create_array_of_titles(array)
   space_in_front = ""
   array.each do |hash|
@@ -10,3 +8,4 @@ def create_array_of_titles(array)
 end
 
 puts create_array_of_titles(buzzfeed_array).inspect
+
