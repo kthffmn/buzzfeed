86ba37535ce28adb33425def4eef87ca84a2b144
Kate <katepdonahue@gmail.com>
2014-02-22 17:38:39 -0500

    Use tsv data and parse into string of titles

---
+++ buzzfeed_parser.rb
@@ -0,0 +1,37 @@
+require 'csv'
+require 'awesome_print'
+require 'debugger'
+
+class CSVParser
+  attr_accessor :output
+  attr_reader :column_names, :data_rows
+
+  def initialize(path_to_tsv)
+    @data_rows = CSV.read(path_to_tsv, { :col_sep => "\t" })
+    @column_names = [:num, :title, :url] # [Timestamp, Date, ...]
+    @output = []
+  end
+
+  def parse
+    data_rows.each do |row|
+      hash = {}  
+      row.each_with_index do |attribute, i|
+        hash[column_names[i]] = attribute
+      end
+      output << hash
+    end
+    output
+  end
+
+  def textify
+    text = ""
+    parse.each do |hash|
+      text << " #{hash[:title]}."
+    end
+    text.gsub("&nbsp;", " ").gsub("&#8217;", "'")
+  end
+
+end
+
+my_parser = CSVParser.new('crawl.tsv')
+print my_parser.textify
