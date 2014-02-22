5592d73c4578d5a61a8ab8a736df6650f1983d7c
Katie Hoffman <ktahoffman@gmail.com>
2014-02-22 15:59:29 -0500

    adds method that grabs the titles

---
+++ grabs_titles.rb
@@ -0,0 +1,10 @@
+
+ARRAY = [{:title=>"35 Nerdy Cards Against Humanity Cards To Add To Your Deck", :items=>"35", :url=>"http://www.buzzfeed.com/kmallikarjuna/nerdy-cards-against-humanity", :shares=>"4K"}, {:title=>"23-Year-Old Woman Grows Beard, Says She Feels More Feminine Than Ever", :items=>"23", :url=>"http://www.buzzfeed.com/alanwhite/23-year-old-woman-grows-beard-says-she-feels-more-feminine-t", :shares=>"230,361"}]
+
+def create_array_of_titles
+  space_in_front = ""
+  ARRAY.each |hash|
+    space_in_front << " '" + hash[title] + "."
+  end
+  titles = space_in_front[1..-1]
+end
