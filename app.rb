3905692521fd44ade93e2d86f171aa077d2ec481
Katie Hoffman <ktahoffman@gmail.com>
2014-02-25 15:01:26 -0500

    fix app to remove redundant logic

--- app.rb
+++ app.rb
@@ -10,13 +10,7 @@ class App < Sinatra::Application
 
   post '/result' do
     my_generator = Generator.new('./titles.txt', params["word"])
-    title = my_generator.sentence
-    tgr = EngTagger.new
-    tagged = tgr.add_tags(title.downcase)
-    nouns_hash = tgr.get_nouns(tagged)
-    gsub_out = nouns_hash.first[0].capitalize
-    gsub_in = params["word"].capitalize
-    @final_title = title.gsub(gsub_out, gsub_in)
+    @title = my_generator.user_title
   end
  
 end
