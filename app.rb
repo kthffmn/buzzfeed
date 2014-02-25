93c90af42c88552ba6c5d60d1ba32ab09240beb6
Kate <katepdonahue@gmail.com>
2014-02-25 14:37:00 -0500

    Add logic to add user input into title

--- app.rb
+++ app.rb
@@ -10,7 +10,14 @@ class App < Sinatra::Application
 
   post '/result' do
     my_generator = Generator.new('./titles.txt')
-    @title = my_generator.sentence
+    title = my_generator.sentence
+    tgr = EngTagger.new
+    text = title
+    tagged = tgr.add_tags(text)
+    nouns_hash = tgr.get_nouns(tagged)
+    gsub_out = nouns_hash.first[0]
+    gsub_in = params["word"]
+    final_title = title.gsub(gsub_out, gsub_in)
   end
  
 end
