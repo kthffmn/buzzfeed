f7f1d7dd284d66ad004f90c6878adaf2aa8ac278
Kate <katepdonahue@gmail.com>
2014-02-25 15:40:14 -0500

    Get the user input word to go into the title

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,22 +1,25 @@
+require 'debugger'
+
 class Generator
   ARRAY = ["Ways To", "Reasons To", "Things To", "", "Things", "Ways"]
-  attr_reader :markov, :word, :tgr
+  attr_reader :markov, :word, :tgr, :marky_title
 
   def initialize(text_file, word)
     file = File.new("./public/#{text_file}", "r")
-    @markov = MarkyMarkov::Dictionary.new('dictionary', 2)
-    @markov.parse_file file
+    markov = MarkyMarkov::Dictionary.new('dictionary', 2)
+    markov.parse_file file
+    @marky_title = markov.generate_1_sentences
     @word = word.capitalize
-    @tgr = EngTagger.new
   end
 
-  def marky_title
-    # sentence = rand(1..36).to_s + " " + ARRAY.sample + " " + @markov.generate_1_sentences
-    # sentence.gsub("  ", " ")
-    @markov.generate_1_sentences
-  end
+  # def marky_title
+  #   # sentence = rand(1..36).to_s + " " + ARRAY.sample + " " + @markov.generate_1_sentences
+  #   # sentence.gsub("  ", " ")
+  #   @markov.generate_1_sentences
+  # end
 
   def user_title
+    tgr = EngTagger.new
     tagged = tgr.add_tags(marky_title.downcase)
     nouns_hash = tgr.get_nouns(tagged)
     gsub_out = nouns_hash.first[0].capitalize
