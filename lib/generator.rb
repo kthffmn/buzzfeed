b23397ddd7880cadab14b28f574909fdb5569525
Katie Hoffman <ktahoffman@gmail.com>
2014-03-02 20:20:17 -0500

    adds pluralization logic to generator.rb along with a loop, also adds boostrap

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,8 +1,5 @@
-require 'debugger'
-
 class Generator
-  # ARRAY = ["Ways To", "Reasons To", "Things To", "", "Things", "Ways"]
-  attr_reader :markov, :word, :tgr, :marky_title
+  attr_reader :markov, :word, :tgr, :marky_title, :in_ends_in_s
 
   def initialize(text_file, word)
     file = File.new("./public/#{text_file}", "r")
@@ -10,20 +7,31 @@ class Generator
     markov.parse_file file
     @marky_title = markov.generate_1_sentences
     @word = word.capitalize
+    @in_ends_in_s = (@word[-1] == "s")
   end
 
-  # def marky_title
-  #   # sentence = rand(1..36).to_s + " " + ARRAY.sample + " " + @markov.generate_1_sentences
-  #   # sentence.gsub("  ", " ")
-  #   @markov.generate_1_sentences
-  # end
-
   def user_title
-    tgr = EngTagger.new
-    tagged = tgr.add_tags(marky_title.downcase)
-    nouns_hash = tgr.get_nouns(tagged)
-    gsub_out = nouns_hash.first[0].capitalize
-    marky_title.gsub(gsub_out, word)
+    title = ""
+    10.times do 
+      tgr = EngTagger.new
+      tagged = tgr.add_tags(marky_title.downcase)
+      nouns_hash = tgr.get_nouns(tagged)
+      if nouns_hash.length > 0
+        gsub_out = nouns_hash.first[0].capitalize
+        out_ends_in_s = (gsub_out[-1] == "s")
+        if out_ends_in_s == in_ends_in_s
+          title = marky_title.gsub(gsub_out, word)
+        elsif out_ends_in_s && !in_ends_in_s
+          final_word = word + "s"
+          title = marky_title.gsub(gsub_out, final_word)
+        else                                              # !out_ends_in_s && in_ends_in_s 
+          final_word = word[0...-1]
+          title = marky_title.gsub(gsub_out, final_word)
+        end
+        break
+      end
+    end
+    title[0...-1]
   end
 
 end
